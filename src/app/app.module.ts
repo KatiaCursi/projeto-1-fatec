755f12d5c759c6417b2b48a62ebae6efdd2a7a4f
Kátia Cursi <cursikatia@gmail.com>
2024-09-17 11:21:18 -0300

    product-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { ProductsTableComponent } from './products-table/products-table.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductFormComponent } from './product-form/product-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     
-    ProductsTableComponent
+    ProductsTableComponent,
+          ProductFormComponent
   ],
   imports: [
     BrowserModule,
